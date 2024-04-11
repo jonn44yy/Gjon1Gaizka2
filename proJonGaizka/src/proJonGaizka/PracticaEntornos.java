60d00e08ae334a52f304a9faa5f386c42f06b99e
Jon <Jon@DESKTOP-VFF4Q6M>
2024-04-11 17:39:02 +0200

    Tercer cambio

--- proJonGaizka/src/proJonGaizka/PracticaEntornos.java
+++ proJonGaizka/src/proJonGaizka/PracticaEntornos.java
@@ -5,6 +5,7 @@ public class PracticaEntornos {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola nuevamente Mundo");
+		System.out.println("Hola tercer Mundo");
 
 	}
 
