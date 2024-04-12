0d420c2111de4fd20bf790f150bd5a461c8b161d
Jon <Jon@DESKTOP-VFF4Q6M>
2024-04-12 12:53:19 +0200

    prueba rama hoy

--- proJonGaizka/src/pro2JonGaizka/practicaEntornos19.java
+++ proJonGaizka/src/pro2JonGaizka/practicaEntornos19.java
@@ -4,6 +4,7 @@ public class practicaEntornos19 {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo ejercicio 21");
+		System.out.println("Hola Mundo prueba");
 	
-}
+	}
 }
