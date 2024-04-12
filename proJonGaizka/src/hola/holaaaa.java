739229fa247e35619896c0aa273b8f60f59895bd
Admin <Admin@DESKTOP-JA67JF7>
2024-04-12 13:18:31 +0200

    13:18 CAMBIO 2

--- proJonGaizka/src/hola/holaaaa.java
+++ proJonGaizka/src/hola/holaaaa.java
@@ -5,6 +5,7 @@ public class holaaaa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Hola NUEVO Mundo");
 	}
 
 }
